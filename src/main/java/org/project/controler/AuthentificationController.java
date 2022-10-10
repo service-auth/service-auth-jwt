c52a7f153756c9435e8ae7187ce03c5def020ee4
msalahe <salaheddine.elmamouni@gmail.com>
2022-10-10 23:37:02 +0000

    créer une base de donne mysql depuis spring

--- src/main/java/org/project/controler/AuthentificationController.java
+++ src/main/java/org/project/controler/AuthentificationController.java
@@ -1,13 +1,28 @@
 package org.project.controler;
 
+import java.util.List;
+
+import org.project.models.User;
+import org.project.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class AuthentificationController {
+	private UserService service;
+	
 	@RequestMapping({ "/hello" })
 	public String firstPage() {
 		return "Hello World";
+	}
+	@GetMapping("/user")
+	public ResponseEntity<List<User>> getUser(){
+		return ResponseEntity.ok().body(service.getUsers());
 	}
 
 }
