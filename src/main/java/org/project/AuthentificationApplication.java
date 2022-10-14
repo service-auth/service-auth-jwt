96c82d34a0af57a98a027032ae2faa204af496c9
msalahe <salaheddine.elmamouni@gmail.com>
2022-10-14 23:01:24 +0000

    http configure

--- src/main/java/org/project/AuthentificationApplication.java
+++ src/main/java/org/project/AuthentificationApplication.java
@@ -17,7 +17,6 @@ public class AuthentificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthentificationApplication.class, args);
-		System.out.println("ss");
 	}
 	
 	@Bean
--- src/main/java/org/project/config/SecurityConfig.java
+++ src/main/java/org/project/config/SecurityConfig.java
@@ -7,6 +7,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter  {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests().anyRequest().permitAll();
+	}
 }
